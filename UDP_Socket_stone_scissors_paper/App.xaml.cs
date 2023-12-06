cf1b623f1516516df8128129aeb3a4a048f0fe3b
RFozzy688 <pokerfozzy@gmail.com>
2023-12-06 15:10:10 +0200

    start

---
+++ UDP_Socket_stone_scissors_paper/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UDP_Socket_stone_scissors_paper
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
