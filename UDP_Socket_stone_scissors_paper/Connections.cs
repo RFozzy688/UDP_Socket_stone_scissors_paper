30d29adf4a69312ddfa779831a78bca9a52f11aa
RFozzy688 <pokerfozzy@gmail.com>
2023-12-07 14:59:41 +0200

    класс для работы с удаленным хостом

---
+++ UDP_Socket_stone_scissors_paper/Connections.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UDP_Socket_stone_scissors_paper
+{
+    public class Connections
+    {
+        Socket _socket;
+        EndPoint _remotePoint;
+
+        public Socket GetSocket {  get { return _socket; } }
+        public EndPoint GetEndPoint { get { return _remotePoint; } }
+        public void SocketClose()
+        {
+            if (_socket.Connected)
+            {
+                _socket.Shutdown(SocketShutdown.Both);
+                _socket.Close();
+            }
+        }
+        public void ConnectTo(string ipAddress, int port) 
+        {
+            SocketClose();
+
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            _remotePoint = new IPEndPoint(IPAddress.Parse(ipAddress), port);
+        }
+        async Task<string> Send(string str)
+        {
+            try
+            {
+                string message = str;
+                byte[] data = new byte[256];
+                data = Encoding.UTF8.GetBytes(message);
+                await _socket.SendToAsync(data, SocketFlags.None, _remotePoint);
+
+                data = new byte[256];
+                var result = await _socket.ReceiveFromAsync(data, SocketFlags.None, _remotePoint);
+                message = Encoding.UTF8.GetString(data, 0, result.ReceivedBytes);
+
+                return message;
+            }
+            catch (SocketException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return string.Empty;
+        }
+    }
+}
